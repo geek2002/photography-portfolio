23c143ec6da9b66162a1e8e6b43779c116d2d58c
geek2002-Laptop <17989022+geek2002@users.noreply.github.com>
2021-07-21 01:39:31 +0100

    Apply better layout to upload form

--- index.php
+++ index.php
@@ -3,36 +3,46 @@
     include "imageProcessing/processing.php";
 ?>
         <div class="container">
-        <form method="POST" action="imageProcessing/upload.php" enctype="multipart/form-data">
-                
-            <div class="form-row">
-                <div class="form-group" id="adminFormGroup">
-                    <label for="uploadedFile">Upload A file:</label>
-                    <input type="file" name="uploadedFile" />
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="image w-100" style="background-image: url(<?php echo substr($_GET['fileName'],3); ?>);"></div>
+                    <p class="mx-auto">
+                        <span style="margin-right: 10px"><i class="fas fa-undo"></i></span>
+                        <span style="margin-right: 10px"><i class="fas fa-undo fa-flip-horizontal"></i></span>
+                    </p>
                 </div>
-                <div class="form-group col-md-12">
-                    <label for="inputEmail4">Title</label>
-                    <input type="text" class="form-control" name="title">
-                </div>
-                <div class="form-group col-md-12">
-                    <label for="location">Location</label>
-                    <input type="text" class="form-control" name="location">
-                </div>
-                <div class="form-group col-md-12">
-                    <label for="price">Price</label>
-                    <input type="text" class="form-control" name="price">
-                </div>
-                <div class="form-group col-md-12">
-                    <label for="catagory">Catagory</label>
-                    <input type="text" class="form-control" name="catagory">
-                </div>
+                <div class="col-md-6">
+                    <form method="POST" action="imageProcessing/upload.php" enctype="multipart/form-data">
+                    
+                    <div class="form-row">
+                        <div class="form-group" id="adminFormGroup">
+                            <label for="uploadedFile">Upload A file:</label>
+                            <input type="file" name="uploadedFile" />
+                        </div>
+                        <div class="form-group col-md-12">
+                            <label for="inputEmail4">Title</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                        <div class="form-group col-md-12">
+                            <label for="location">Location</label>
+                            <input type="text" class="form-control" name="location">
+                        </div>
+                        <div class="form-group col-md-12">
+                            <label for="price">Price</label>
+                            <input type="text" class="form-control" name="price">
+                        </div>
+                        <div class="form-group col-md-12">
+                            <label for="catagory">Catagory</label>
+                            <input type="text" class="form-control" name="catagory">
+                        </div>
+                    </div>
+                    
+                    <button name="uploadBtn" type="submit" value="Upload" class="btn btn-primary">Upload</button>
+                </form>
             </div>
-            
-            <button name="uploadBtn" type="submit" value="Upload" class="btn btn-primary">Upload</button>
-        </form>
+            </div>
+        
         </div>
-        <div class="image mx-auto" style="background-image: url(<?php echo substr($_GET['fileName'],3); ?>);">
-
-        </div>
+        
     </body>
 </html>
