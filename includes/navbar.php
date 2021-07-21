e17c522797a05d29818fa9219617a1acee825df1
geek2002-Laptop <17989022+geek2002@users.noreply.github.com>
2021-07-21 01:40:49 +0100

    Add locations to links.
    - Add comments for links for bootstrap and font awesome
    - Add margin to bottom of navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,19 +3,21 @@
     <head>
         <title>PHP File Upload</title>
         <link rel="stylesheet" href="Custom Css/style.css">
+
+        <!-- Bootstrap -->
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <script src="bootstrap/js/jquery.min.js"></script>
         <script src="bootstrap/js/popper.min.js"></script>
         <script src="bootstrap/js/bootstrap.min.js"></script>
 
+        <!-- Font Awesome -->
         <link href="fonts/fontawesome/css/fontawesome.css" rel="stylesheet">
         <link href="fonts/fontawesome/css/brands.css" rel="stylesheet">
         <link href="fonts/fontawesome/css/solid.css" rel="stylesheet">
     </head>
     <body>
-
-        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-secondary">
-            <a class="navbar-brand" href="#">
+        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-secondary" style="margin-bottom: 20px;">
+            <a class="navbar-brand" href="index.php">
                 <img src="https://getbootstrap.com/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
             </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,10 +26,10 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Gallery</a>
+                        <a class="nav-link" href="gallery.php">Gallery</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Bulk Ordering</a>
@@ -37,5 +39,4 @@
                     </li>
                     </ul>
                 </div>
-            </div>
         </nav>
