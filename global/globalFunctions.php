3a7169eb3301b85f1b7f2d15053c1141550a8483
geek2002-Laptop <17989022+geek2002@users.noreply.github.com>
2021-07-23 00:10:50 +0100

    Create globalFunctions.php

---
+++ global/globalFunctions.php
@@ -0,0 +1,27 @@
+<?php
+    include "databaseFunctions.php";
+    function globalDeleteAllImages($folder){
+        include "varables.php";
+        echo "Deleting folder: " . $folder . "<br>";
+        $files = glob('../' . $globalUploadLocation . '/' . $folder . '/*'); // get all file names
+        foreach($files as $file){ // iterate files
+            if(is_file($file)) {
+                echo "  Deleting: " . $file . "<br>";
+                unlink($file); // delete file
+            }
+        }
+    }
+    $function = $_POST['function'];
+    echo "Function: " . $function . "<br>";
+    switch ($function) {
+        case 'globalDeleteAllImages':
+            globalDeleteAllImages("original");
+            globalDeleteAllImages("preview");
+            globalDeleteAllImages("thumnail");
+            clearTable("photos");
+            break;
+        default:
+            # code...
+            break;
+    }
+?>
