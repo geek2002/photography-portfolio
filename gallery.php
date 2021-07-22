e22d678d3b5c2090ecdd414f86f73ac4c335ce7d
geek2002 <17989022+geek2002@users.noreply.github.com>
2021-07-22 17:04:43 +0100

    Create Gallery.php

--- gallery.php
+++ gallery.php
@@ -1,3 +1,37 @@
 <?php
     include "includes/navbar.php";
+    include "global/databaseFunctions.php";
 ?>
+
+        <div class="container">
+            <div class="row">
+        <?php
+            $data = getDatabaseData("select * from photos INNER JOIN categories ON photos.photo_cata_id = categories.cata_id ORDER BY photo_id");
+            $rows = $data->rowCount();
+            while($row = $data->fetch(PDO::FETCH_ASSOC)){
+                $width = intval($row['photo_width']);
+                $height = intval($row['photo_height']);
+                $resolution = ceil(($width * $height)/1000000);
+                echo "<div class='col-md-6'>";
+                echo "    <div class='card'>";
+                echo "        <div class='galleryImg' style='background-Image: url(" . "uploaded-Images/preview/" . $row['photo_uploadedFileName'] . ".jpg')'></div>";
+                echo "        <div class='card-body'>";
+                echo "            <h6>" . $row['photo_title'] . "</h6>";
+                echo "            <ul style='list-style-type: none; padding:0px'>";
+                echo "                <li><h7><span><i class='fas fa-camera'></i></span> " . $resolution . "MP (" . $row['photo_width'] . " x " . $row['photo_height'] . ") </h7></li>";
+                echo "                <li><h7><span><i class='fas fa-map-marker-alt'></i></span> " . $row['photo_location'] . "</h7></li>";
+                echo "                <li><h7><span><i class='fas fa-th'></i></span> " . $row['cata_name'] . "</h7></li>";
+                echo "                <li><h7><span><i class='fas fa-tag'></i></span> £" . $row['photo_price'] . "</h7></li>";
+                echo "            </ul>";
+                echo "            <a href='viewImage.php?image=" . $row['photo_uploadedFileName'] . "'><button type='button' class='btn btn-primary btn-lg btn-block'>View Image</button></a>";
+                echo "         </div>";
+                echo "    </div>";
+                echo "</div>";
+            }
+        ?>
+                    
+        </div>
+    
+    </body>
+</html>
+
