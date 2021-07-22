5a8ebc353a51120e331d411c5b60af8deefe17c0
geek2002-Laptop <17989022+geek2002@users.noreply.github.com>
2021-07-22 23:32:05 +0100

    Delete databaseConnection.php

--- global/databaseConnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $hostname = "localhost"; //where is the db - localhost means on your machine or server
-    $databasename = "photography"; //name of our database
-    $username = "Steven"; //username to log in with
-    $password = "0elnb598OgTIE3ZO"; //password for the user
-
-    $pdo = new PDO("mysql:host=$hostname;dbname=$databasename", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-?>
