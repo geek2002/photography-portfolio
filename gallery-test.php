9b703cedd1261a9da322cdcd6b35afd642a24642
geek2002 <17989022+geek2002@users.noreply.github.com>
2021-07-22 17:02:43 +0100

    Create gallery-test.php

---
+++ gallery-test.php
@@ -0,0 +1,108 @@
+<?php
+    include "includes/navbar.php";
+?>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="galleryImg" style="background-Image: url('uploaded-Images/original/bb83b96669426937836b4393f3755f37.jpg')"></div>
+                        
+                        <div class="card-body">
+                            <h6>IMG_5747 - Edit [2021-04-25].jpg</h6>
+                            <ul style="list-style-type: none; padding:0px">
+                                <li><h7><span><i class="fas fa-camera"></i></span> 24 MP 6000x4000</h7></li>
+                                <li><h7><span><i class="fas fa-map-marker-alt"></i></span> Unassigned</h7></li>
+                                <li><h7><span><i class="fas fa-th"></i></span> Cars</h7></li>
+                                <li><h7><span><i class="fas fa-tag"></i></span> £4.99</h7></li>
+                            </ul>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="galleryImg" style="background-Image: url('uploaded-Images/original/93118415619672ff7f1311de9484223e.jpg')"></div>
+                        <div class="card-body">
+                            <h6>IMG_5747 - Edit [2021-04-25].jpg</h6>
+                            <ul style="list-style-type: none; padding:0px">
+                                <li><h7><span><i class="fas fa-camera"></i></span> 24 MP 6000x4000</h7></li>
+                                <li><h7><span><i class="fas fa-map-marker-alt"></i></span> Unassigned</h7></li>
+                                <li><h7><span><i class="fas fa-th"></i></span> Cars</h7></li>
+                                <li><h7><span><i class="fas fa-tag"></i></span> £4.99</h7></li>
+                            </ul>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="padding-top: 20px;">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="galleryImg" style="background-Image: url('uploaded-Images/original/bb83b96669426937836b4393f3755f37.jpg')"></div>
+                        <div class="card-body">
+                            <h6>IMG_5747 - Edit [2021-04-25].jpg</h6>
+                            <ul style="list-style-type: none; padding:0px">
+                                <li><h7><span><i class="fas fa-camera"></i></span> 24 MP 6000x4000</h7></li>
+                                <li><h7><span><i class="fas fa-map-marker-alt"></i></span> Unassigned</h7></li>
+                                <li><h7><span><i class="fas fa-th"></i></span> Cars</h7></li>
+                                <li><h7><span><i class="fas fa-tag"></i></span> £4.99</h7></li>
+                            </ul>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </div>
+            <div class="row" style="padding-top: 20px;">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="galleryImg" style="background-Image: url('uploaded-Images/original/93118415619672ff7f1311de9484223e.jpg')"></div>
+                        <div class="card-body">
+                            <h6>IMG_5747 - Edit [2021-04-25].jpg</h6>
+                            <ul style="list-style-type: none; padding:0px">
+                                <li><h7><span><i class="fas fa-camera"></i></span> 24 MP 4000x6000</h7></li>
+                                <li><h7><span><i class="fas fa-map-marker-alt"></i></span> Unassigned</h7></li>
+                                <li><h7><span><i class="fas fa-th"></i></span> Cars</h7></li>
+                                <li><h7><span><i class="fas fa-tag"></i></span> £4.99</h7></li>
+                            </ul>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="galleryImg" style="background-Image: url('uploaded-Images/original/066387b09d62e68d23fd35a3c64c5a8b.jpg')"></div>
+                        <div class="card-body">
+                            <h6>IMG_5747 - Edit [2021-04-25].jpg</h6>
+                            <ul style="list-style-type: none; padding:0px">
+                                <li><h7><span><i class="fas fa-camera"></i></span> 24 MP 4000x6000</h7></li>
+                                <li><h7><span><i class="fas fa-map-marker-alt"></i></span> Unassigned</h7></li>
+                                <li><h7><span><i class="fas fa-th"></i></span> Cars</h7></li>
+                                <li><h7><span><i class="fas fa-tag"></i></span> £4.99</h7></li>
+                            </ul>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="galleryImg" style="background-Image: url('uploaded-Images/original/93118415619672ff7f1311de9484223e.jpg')"></div>
+                        <div class="card-body">
+                            <h6>IMG_5747 - Edit [2021-04-25].jpg</h6>
+                            <ul style="list-style-type: none; padding:0px">
+                                <li><h7><span><i class="fas fa-camera"></i></span> 24 MP 4000x6000</h7></li>
+                                <li><h7><span><i class="fas fa-map-marker-alt"></i></span> Unassigned</h7></li>
+                                <li><h7><span><i class="fas fa-th"></i></span> Cars</h7></li>
+                                <li><h7><span><i class="fas fa-tag"></i></span> £4.99</h7></li>
+                            </ul>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
