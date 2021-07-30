3afb0a8cf4826380a2a7e394eb8107e87b62886a
geek2002 <17989022+geek2002@users.noreply.github.com>
2021-07-30 02:12:37 +0100

    Create countPhotos Function

--- global/databaseFunctions.php
+++ global/databaseFunctions.php
@@ -19,5 +19,28 @@
         } catch (PDOException $errorMessage) {
             echo $errorMessage;
         }
+    }
+    function countPhotos($orientation=null){
+        if ($orientation == null){
+            $sql = "SELECT * FROM photos;"; 
+        }else{         
+            $sql = "SELECT * FROM photos WHERE photo_orientation='" . $orientation . "';"; 
+        }
+        $ids = array();
+        try {
+            include "databaseConnection.php";     
+            $stmt = $pdo->query($sql);
+            if($stmt->rowCount()){
+                $data = $stmt;
+                while($row = $data->fetch(PDO::FETCH_ASSOC)){
+                    array_push($ids,$row['photo_id']);
+                }
+                $returnData = array($stmt->rowCount(), $ids);
+                // print_r($returnData[1]);
+                return $returnData;
+            }
+        } catch (PDOException $errorMessage) {
+            echo $errorMessage;
+        }
     }    
 ?>
