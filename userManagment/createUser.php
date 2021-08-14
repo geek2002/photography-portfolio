82175d944ec251161a76cbe6d06d4b97ac0f5406
geek2002-Laptop <17989022+geek2002@users.noreply.github.com>
2021-08-14 16:30:23 +0100

    Create Login System

--- userManagment/createUser.php
+++ userManagment/createUser.php
@@ -1,61 +1,55 @@
 <?php
-    include "../global/databaseFunctions.php";
-    include "../global/varables.php";
+    include "../global/globalFunctions.php";
 
-    function createRandomUserID(){
-        while (true){
-            $tempID = rand(0,9999999999);
-            $available = checkID($tempID);
-            if ($available == true){
-                echo $tempID . "<br>";
-                return $tempID;
-                break;
-            }
-        }
+    
+    $inId = createRandomUserID();
+    $inEmail = $_POST['email'];
+    $inUsername = $_POST['username'];
+    $inPassword = $_POST['password'];
+    $inFname = $_POST['fname'];
+    $inLname = $_POST['lname'];
+    $inUserType = $_POST['userType'];
+
+    $hashedPassword = password_hash($inPassword , PASSWORD_DEFAULT);
+    echo "ID: " . $inId . "<br>";
+    echo "Email: " . $inEmail . "<br>";
+    echo "Username: " . $inUsername . "<br>";
+    echo "Password: " . $inPassword . "<br>";
+    echo "Hashed Password: " . $hashedPassword . "<br>";
+    echo "First Name: " . $inFname . "<br>";
+    echo "Last Name: " . $inLname . "<br>";
+    echo "User Type: " . $inUserType . "<br>";
+
+    if (checkEmail($inEmail)) {
+       echo "Email Available";
+    }else{
+        echo "Email already in use";
+        global_back("Email Already in use");
+        exit;
     }
 
-    // $new_user_id = createRandomUserID();
-    // echo $new_user_id;
-
-
-
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $action = $_POST['action'];
     try
     {
         include "../global/databaseConnection.php";
         $data = [
-            'newuserid' => createRandomUserID(),
-            'email' => $username,
-            'username' => $username,
-            'password' => $password,
-            'fname' => $fname,
-            'lname' => $lname
+            'newuserid' => $inId,
+            'email' => $inEmail,
+            'username' => $inUsername,
+            'password' => $hashedPassword,
+            'fname' => $inFname,
+            'lname' => $inLname,
+            'userType' => $inUserType
         ];
-        $sql = "INSERT INTO users(userID, user_email, user_username, user_password, user_fname, user_lname  ) VALUES(:newuserid,:email,:username,:password,:fname,:lname)";
-        // $sql = "INSERT INTO users (name, surname, sex) VALUES (:name, :surname, :sex)";
+        $sql = "INSERT INTO users(userID, user_email, user_username, user_password, user_fname, user_lname, user_type) VALUES(:newuserid,:email,:username,:password,:fname,:lname,:userType)";
         $stmt= $pdo->prepare($sql);
         $stmt->execute($data);
-        // if($runstatement)
-        // {
-        //     echo "Data entered into the database";
-        //     if($action == "login"){
-        //         session_id(1);
-        //         session_start();
-        //         $_SESSION['email'] = $email;
-        //         $_SESSION['password'] = $password;
-        //         header("Location: login.php");  
-        //     }
-        // }
-    }
-    catch(PDOException $errormessage)
-    {
+        if ($stmt){
+            echo "Data entered into the database sucsessfully";
+
+        }
+
+    }catch(PDOException $errormessage){
         echo "An error has occured, see the error details below <br>";
         echo $errormessage;
     }
-    
 ?>
--- userManagment/login.php
+++ userManagment/login.php
@@ -1,10 +1,43 @@
 <?php
-    session_id(1);
-    session_start();
-    $email = $_SESSION['email'];
-    $password = $_SESSION['password'];
-    session_destroy();
+    
 
-    echo "Email: " . $email;
-    echo "Password: " . $password;
+    $inEmail = $_POST['email'];
+    $inPassword = $_POST['password'];
+
+    echo "Input Email: " . $inEmail . "<br>";
+    echo "Input Password: " . $inPassword . "<br>";
+
+    try {
+        include "../global/databaseConnection.php";
+        $sql = "SELECT * FROM users WHERE user_email = '" . $inEmail . "'";
+        $stmt = $pdo->query($sql);
+        if($stmt->rowCount()){
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            echo "Fetched Data <br>";
+            print_r($row);
+            echo "<br>";
+
+            if (password_verify($inPassword, $row['user_password'])) {
+                echo 'Password is valid!';
+                echo "Checking User Type";
+                if ($row['user_type'] == 0) {
+                    echo "Normal user";
+                    header("Location: ../userPages/normal.php");
+                    exit;
+                }elseif ($row['user_type'] == 1) {
+                    echo "Admin user";
+                    header("Location: ../userPages/admin.php");
+                    exit;
+                }
+                echo "Redirecting...";
+
+            } else {
+                echo 'Invalid password.';
+            }
+        }else{
+            return "No Data";
+        }
+    } catch (PDOException $errorMessage) {
+        echo $errorMessage;
+    }
 ?>
