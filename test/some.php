d0e3cdeab727665e7428e7f1389e7175b38c9825
geek2002-Laptop <17989022+geek2002@users.noreply.github.com>
2021-09-23 19:27:53 +0100

    Files Test

---
+++ test/some.php
@@ -0,0 +1,10 @@
+<?php
+    function abc($name){
+        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        $txt = "John Doe\n";
+        fwrite($myfile, $txt);
+        $txt = "Jane Doe\n";
+        fwrite($myfile, $txt);
+        fclose($myfile);
+    }
+?>
