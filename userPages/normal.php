0bd1ab05bdfcaac0065ba7db6b899c6a84279a80
geek2002-Laptop <17989022+geek2002@users.noreply.github.com>
2021-10-29 17:01:30 +0100

    Add kick when not logged in

--- userPages/normal.php
+++ userPages/normal.php
@@ -1,8 +1,16 @@
 <?php
-    // session_start();
-    include "../includes/navbar.php";
+    session_start();
+    $rootLocation = "../";
     include "../global/varables.php";
     include "../global/databaseFunctions.php";
+    include "../global/globalFunctions.php";
+    include "../includes/navbar.php";
+
+    if(isset($_SESSION['userID']) == false){
+        header("Location: ../?error=unauth");
+        exit;
+        echo "NOT";
+    }
 ?>
 
         <div class="container">
